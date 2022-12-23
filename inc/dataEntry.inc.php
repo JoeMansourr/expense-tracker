d025005906cb78eb925f26f1ab589ca587827461
Joe Mansour <joemansourr@outlook.com>
2022-12-23 14:49:25 +0200

    Update dataEntry.inc.php

--- inc/dataEntry.inc.php
+++ inc/dataEntry.inc.php
@@ -5,14 +5,6 @@
     if(isset($_POST["submit"])){
         $itemName = $_POST['itemName'];
         $itemPrice = $_POST['itemPrice'];
-
-        // if($itemPrice < 0){
-        //     $sql = "insert into negativevalues (negativeValues) values ('$itemPrice')";
-        //     $result = mysqli_query($conn, $sql);
-        //     if($result){
-        //         header("Location: ../index.php");
-        //     }
-        // }
         
             $sql = "insert into itemstb (item_name, price) values ('$itemName', '$itemPrice')";
             $result = mysqli_query($conn, $sql);
